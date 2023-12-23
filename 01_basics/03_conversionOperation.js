df7d0c6d92f09a43a36d3d548b5a9d6cab45c413
sameerfs <154583934+sameerfs@users.noreply.github.com>
2023-12-23 06:58:41 +0000

    Convesion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+// let score = 33
+// let score = "33"
+// let score = "33aa"
+// let score = true
+let score
+
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+//Number Conversion
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Number Conversion output example
+// "33" => 33
+// 33 => 33
+// "33aa" => NaN
+// true => 1, false => 0
+// undefined => NaN
+
+// boolean Conversion
+let isLoggedIn;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true 0 => fasle
+// "22" => true
+// "" => false
+// true => true, false => fasle
+// undefined => false
+
+//string conversion
+let cityName
+let stringConver = String(cityName)
+
+console.log(cityName);
+console.log(typeof stringConver);
+console.log(stringConver);
+
+// "" => 
+// "33abc" => 33abc
+// true => true,  false => false
+// undefiend => undefend
