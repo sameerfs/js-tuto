02b5d05cf316b3f5f300c7892dac91ba92867325
sameerfs <154583934+sameerfs@users.noreply.github.com>
2023-12-23 07:47:28 +0000

    Conversion and Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,11 +39,60 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let cityName
 let stringConver = String(cityName)
 
-console.log(cityName);
-console.log(typeof stringConver);
-console.log(stringConver);
+// console.log(cityName);
+// console.log(typeof stringConver);
+// console.log(stringConver);
 
 // "" => 
 // "33abc" => 33abc
 // true => true,  false => false
 // undefiend => undefend
+
+/****************************Operations*************************************/
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let string1 = "ASWK"
+let string2 = " Taha Bhai"
+
+let string3 = string1 + string2
+// console.log(string3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+let gameCounter2 = ++gameCounter;
+
+console.table([gameCounter, gameCounter2])
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//prefixed and post fixed operator read on mdn
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+let X = 100
+let Y = X++;
+console.table([X, Y])
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+let X2 = 100
+let Y2 = ++X2;
+console.table([X2, Y2])
